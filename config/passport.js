1a3969aacff734bb78abffb7932f90bd7ff11a58
shyam-shaji1 <shyamshaji42@gmail.com>
2024-12-23 17:09:38 +0530

    google auth url change

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const env = require('dotenv').config();
 passport.use(new googleStrategy({
     clientID : process.env.GOOGLE_CLIENT_ID,
     clientSecret : process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL : 'http://localhost:3000/auth/google/callback'
+    callbackURL : 'https://watchhub.life/auth/google/callback'
 },
 
 async (accessToken,refreshToken,profile,done)=>{
